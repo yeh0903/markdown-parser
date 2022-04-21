667bb86a33a114684f7c8aaa984ac8ca09c2ed4a
yeh0903 <joseph.ycx@gmail.com>
2022-04-21 10:45:21 -0700

    Update

---
+++ MarkdownParseTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testMarkdownParse1() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(links, List.of("https://something.com", "some-thing.html"));
+    }
+}
