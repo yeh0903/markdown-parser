cad0554da1137092680a8680efe725a1a4de82c0
yeh0903 <joseph.ycx@gmail.com>
2022-04-24 12:28:54 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,12 +16,12 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
             if (openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1) {
                 currentIndex = currentIndex + 1;
                 continue;
             }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
         return toReturn;
     }
